6f2d33c4457d6ca0c346c156daebf07a9ca0e4b8
Allaloubouchra <bouchra.allalou10@gmail.com>
2022-06-16 21:21:06 +0200

    'changes'

--- centreapp/urls.py
+++ centreapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
-from centreapp.views import LogInApi, SurveyViewSet, RegisterApi
+from centreapp.views import LogInApi, SurveyViewSet, RegisterApi, CenterViewSet
 
 router = SimpleRouter()
 router.register('survey', SurveyViewSet, )
+router.register('center', CenterViewSet, )
 urlpatterns = [
     path('login/', LogInApi.as_view()),
     path('register/', RegisterApi.as_view(),),
--- centreapp/views.py
+++ centreapp/views.py
@@ -2,11 +2,12 @@ from django.forms import model_to_dict
 from rest_framework import generics, status
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
 
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from centreapp.models import Survey
-from centreapp.serializers import SurveySerializer
+from centreapp.models import Survey, VaccineCentre, Vaccine
+from centreapp.serializers import SurveySerializer, VaccineCentreSerializer, VaccineSerializer
 
 
 class UserCreateAPIView(generics.CreateAPIView):
@@ -34,7 +35,8 @@ class LogInApi(ObtainAuthToken):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
-        data = {'token': token.key}
+        data = {'token': token.key,
+                'user_type': user.account.user_type}
         data.update(model_to_dict(user, exclude=["password", "user_permissions", "is_staff", "is_active"]))
         try:
             data.update(model_to_dict(user.account, fields="__all__"))
@@ -46,6 +48,22 @@ class LogInApi(ObtainAuthToken):
 class SurveyViewSet(ModelViewSet):
     serializer_class = SurveySerializer
     queryset = Survey.objects.all()
-
     # todo limit access for doctors and centre doctors
 
+class CenterViewSet(ModelViewSet):
+    serializer_class = VaccineCentreSerializer
+    queryset = VaccineCentre.objects.all()
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        queryset = super(CenterViewSet, self).get_queryset()
+        return queryset
+
+
+
+class VaccinViewSet(ModelViewSet):
+    serializer_class = VaccineSerializer
+    queryset = Vaccine.objects.all()
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        queryset = super(VaccinViewSet, self).get_queryset()
+        return queryset
